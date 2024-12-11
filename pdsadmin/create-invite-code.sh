620c9c0515e84ad5682398810a49f6f1c2d17f92
Orual <orual.nbc@proton.me>
2024-12-11 14:38:01 -0500

    Update create-invite-code.sh

--- pdsadmin/create-invite-code.sh
+++ pdsadmin/create-invite-code.sh
@@ -3,8 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-PDS_ENV_FILE=${PDS_ENV_FILE:-"/home/orual/pds/pds.env"}
-source "${PDS_ENV_FILE}"
+PDS_ENV_FILE=${PDS_ENV_FILE:-"/pds/pds.env"}
+export $(cat "${PDS_ENV_FILE}" | grep -v ^# | xargs) >/dev/null
 
 curl \
   --fail \
