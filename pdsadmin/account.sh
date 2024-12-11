6dfcb9e90ebc24897f348c4b59e52155f5b54a80
Orual <orual.nbc@proton.me>
2024-12-11 13:30:16 -0500

    Update account.sh

--- pdsadmin/account.sh
+++ pdsadmin/account.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o pipefail
 
 PDS_ENV_FILE=${PDS_ENV_FILE:-"/home/orual/pds/pds.env"}
-source "${PDS_ENV_FILE}"
+export $(cat "${PDS_ENV_FILE}" | grep -v ^# | xargs) >/dev/null
 
 
 # curl a URL and fail if the request fails.
